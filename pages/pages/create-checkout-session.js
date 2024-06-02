eed7783679aae0c8e608e627cded6ad3189c5141
gehlenbr <matheusgehlenbr@icloud.com>
2024-06-02 08:55:13 -0300

    Update create-checkout-session.js

--- pages/pages/create-checkout-session.js
+++ pages/pages/create-checkout-session.js
@@ -1,37 +1,35 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event) => {
-  const { valorConta, parcelas, valorParcela, valorFinal } = JSON.parse(event.body);
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { valorConta, parcelas, valorParcela, valorFinal } = req.body;
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: 'brl',
-            product_data: {
-              name: 'Pagamento de Aluguel',
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'brl', // ou a moeda que você está usando
+              product_data: {
+                name: 'Pagamento de Conta',
+              },
+              unit_amount: Math.round(valorFinal * 100), // valor em centavos
             },
-            unit_amount: Math.round(valorFinal * 100), // em centavos
+            quantity: 1,
           },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `${process.env.URL}/success`,
-      cancel_url: `${process.env.URL}/cancel`,
-    });
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/cancel`,
+      });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ id: session.id }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
+      res.status(200).json({ id: session.id });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
-};
-
+}
